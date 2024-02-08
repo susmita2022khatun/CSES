10f36c1ccc400f35263c87ea66bdf4d9292685e1
susmita2022khatun <susmitakhatun2004@gmail.com>
2024-02-08 13:27:01 +0530

    day3

---
+++ python/hanoi.py
@@ -0,0 +1,26 @@
+def hanoi(n, source, target, auxiliary):
+    if n == 1:
+        return 1
+    else:
+        count = 0
+        count += hanoi(n - 1, source, auxiliary, target)
+        count += 1
+        count += hanoi(n - 1, auxiliary, target, source)
+        return count
+    
+def step(n, source, target, auxiliary):
+    if n == 1:
+        print(source, target)
+        return 1
+    else:
+        count = 0
+        count += step(n - 1, source, auxiliary, target)
+        print(source, target)
+        count += 1
+        count += step(n - 1, auxiliary, target, source)
+        return count
+
+n = int(input())
+print( hanoi(n, 1, 3, 2))
+
+step(n, 1, 3, 2)
